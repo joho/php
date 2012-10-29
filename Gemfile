a7d64910a11e7349300311aaf49dee106680e279
John Barton (joho) <jrbarton@gmail.com>
2012-10-30 09:58:41 +1000

    Switch to using compass/scss for styles.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source :rubygems
 
 gem 'sinatra', '1.3.0'
 gem 'thin', '1.2.7'
+gem 'compass'
+gem 'sass'
 
 group :development do
   gem 'heroku', '2.19.2'
--- web.rb
+++ web.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
+require 'compass'
+require 'sass'
 
+# Asset settings
 set :public_folder, Proc.new { File.join(root, "public") }
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+end
+# End asset settings
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
-
