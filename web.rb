5cc3430eb39b41e9db96279934fdee5894af988b
John Barton (joho) <jrbarton@gmail.com>
2012-10-30 16:53:21 +1000

    Well, that's enough self quoting for now.
    I don't want to accidentally turn into @benschwarz.

--- web.rb
+++ web.rb
@@ -11,17 +11,24 @@ configure do
 end
 # End asset settings
 
+class ExternalLink < Struct.new(:title, :url)
+  def self.recent_writings
+    [
+      new("Interview for The Fetch", "http://blog.thefetch.com/2012/10/29/interview-melbourne-local-john-barton/"),
+      new("The Minimum Viable Rails Stack", "http://goodfil.ms/blog/posts/2012/09/15/minimum-viable-rails-stack/"),
+      new("Will App.net be the connective tissue founders & developers can rely on?", "http://thenextweb.com/apps/2012/08/18/why-10000-people-care-app-net/"),
+      new("Scaling Rails @ Melbourne Ruby Meetup", "http://jrb.tumblr.com/post/30570014929/scaling-rails-at-melbourne-roro")
+    ]
+  end
+end
+
 get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
 end
 
 get '/' do
-  @recent_writings = []
-  @recent_writings << ["Interview for The Fetch", "http://blog.thefetch.com/2012/10/29/interview-melbourne-local-john-barton/"]
-  @recent_writings << ["The Minimum Viable Rails Stack", "http://goodfil.ms/blog/posts/2012/09/15/minimum-viable-rails-stack/"]
-  @recent_writings << ["Will App.net be the connective tissue founders & developers can rely on?", "http://thenextweb.com/apps/2012/08/18/why-10000-people-care-app-net/"]
-  @recent_writings << ["Scaling Rails @ Melbourne Ruby Meetup", "http://jrb.tumblr.com/post/30570014929/scaling-rails-at-melbourne-roro"]
+  @recent_writings = ExternalLink.recent_writings
 
   @interesting_projects = []
   @interesting_projects << ["This site (Sinatra + SCSS)", "https://github.com/joho/whoisjohnbarton.com"]
