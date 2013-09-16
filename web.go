600c5ebe2b10643b91660db23749d3296b9f873d
John Barton (joho) <jrbarton@gmail.com>
2013-09-16 20:32:13 +1000

    Move my totally pro http handler into a package.

--- web.go
+++ web.go
@@ -3,39 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/joho/whoisjohnbarton/homepage"
+	"github.com/joho/whoisjohnbarton/prohttphandler"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
-
-type proHttpHandler struct {
-	fsHandler             http.Handler
-	exactMatchHandleFuncs map[string]func(http.ResponseWriter, *http.Request)
-}
-
-func (h *proHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handleFunc, handleFuncPresent := h.exactMatchHandleFuncs[r.URL.Path]
-	if handleFuncPresent == true {
-		handleFunc(w, r)
-	} else if !strings.HasSuffix(r.URL.Path, "/") {
-		h.fsHandler.ServeHTTP(w, r)
-	} else {
-		http.NotFound(w, r)
-	}
-}
-
-func (h *proHttpHandler) ExactMatchFunc(path string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-	h.exactMatchHandleFuncs[path] = handlerFunc
-}
-
-func newProHttpHandler() (h *proHttpHandler) {
-	h = new(proHttpHandler)
-	h.exactMatchHandleFuncs = map[string]func(http.ResponseWriter, *http.Request){}
-	h.fsHandler = http.FileServer(http.Dir("public"))
-	return
-}
 
 func main() {
 	portNumber := os.Getenv("PORT")
@@ -44,7 +17,7 @@ func main() {
 	}
 	listenOn := fmt.Sprintf(":%v", portNumber)
 
-	handler := newProHttpHandler()
+	handler := prohttphandler.New()
 	handler.ExactMatchFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := homepage.New()
 		t, err := template.ParseFiles("views/index.html")
